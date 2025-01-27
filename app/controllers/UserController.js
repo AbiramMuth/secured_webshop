da336a3326b8c63de4a370da35df91e6d13c104c
AbiramMuth <160613649+AbiramMuth@users.noreply.github.com>
2025-01-27 13:30:58 +0100

    update : page login + register + user
    affichage de la page login et register.

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,9 +1,14 @@
 module.exports = {
-    // https://localhost/user
-    get: (req, res) => {
-        res.send("Bienvenu dans la page Utilisateur");
-    },
-    login: (req, res) => {
-       
-    }
+  // https://localhost/user
+  get: (req, res) => {
+    res.sendfile("/public/view/user.html", { root: "." });
+  },
+  // https://localhost/user/login
+  login: (req, res) => {
+    res.sendfile("/public/view/login.html", { root: "." });
+  },
+  // https://localhost/user/register
+  register: (req, res) => {
+    res.sendfile("/public/view/register.html", { root: "." });
+  },
 };
--- app/server.js
+++ app/server.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const https = require('https');
- 
-const sslOptions = {    
-    key: fs.readFileSync("./privkey.key"),    
-    cert: fs.readFileSync("./certificate.crt")
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const https = require("https");
+
+const sslOptions = {
+  key: fs.readFileSync("./privkey.key"),
+  cert: fs.readFileSync("./certificate.crt"),
 };
- 
+
 const app = express();
-const userRoute = require('./routes/User');
-app.use('/user', userRoute);
- 
-app.get('/user/login',function(req,res){
-    res.sendFile(path.join(__dirname+'/public/login.html'));
-  });
- 
+const userRoute = require("./routes/User");
+app.use(express.static("public"));
+app.use("/user", userRoute);
+
 https.createServer(sslOptions, app).listen(443, () => {
-    console.log('Server running on port 443');
+  console.log("Server running on port 443");
 });
